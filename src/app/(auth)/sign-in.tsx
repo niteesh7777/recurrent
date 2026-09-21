777d5fd57e9fa60703f5116877da8c0aa8c1e8a0
Nitee Naik <niteeshnaik77@gmail.com>
2026-09-21 20:14:32 +0530

    set up and load custom fonts

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -5,10 +5,10 @@ import { Text, View } from "react-native";
 const SignIn = () => {
   return (
     <AreaSafeView>
-      <View className="flex-1 justify-center items-center ">
+      <View className="flex-1 justify-center items-center  ">
         <Text>SignIn</Text>
         <Link href={"/"} className="mt-4 bg-primary p-4 text-white rounded">
-          Go Back
+          Go Home
         </Link>
       </View>
     </AreaSafeView>
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -7,22 +7,22 @@ import { Text } from "react-native";
 export default function Index() {
   return (
     <AreaSafeView >
-      <Text className="text-xl font-bold">Wellcome to home screen</Text>
+      <Text className="text-7xl font-sans-bold">Home</Text>
       <Link
         href={"/onboarding"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold text-center"
       >
         Go To Onboarding
       </Link>
       <Link
         href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold text-center"
       >
         SignIn
       </Link>
       <Link
         href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold text-center"
       >
         SingUp
       </Link>
@@ -31,7 +31,7 @@ export default function Index() {
           pathname: "/subscriptions/[id]",
           params: { id: "spotify" },
         }}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold text-center"
       >
         spotify subscription
       </Link>
@@ -40,7 +40,7 @@ export default function Index() {
           pathname: "/subscriptions/[id]",
           params: { id: "claude" },
         }}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold text-center"
       >
         claude subscription
       </Link>
